5aebe7d8d524ebee7d6018a5f7eb47007d5f7754
Osman <osman.humayun@gmail.com>
2023-09-22 15:53:27 -0700

    1

--- app.py
+++ app.py
@@ -5,13 +5,19 @@ from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain, SequentialChain
 from langchain.memory import ConversationBufferMemory
 from langchain.utilities import WikipediaAPIWrapper
+from langchain.tools import Tool
+from langchain.utilities import GoogleSearchAPIWrapper
 
 os.environ['OPENAI_API_KEY'] = st.secrets['OPENAI_API_KEY']
+
+os.environ["GOOGLE_CSE_ID"] = st.secrets['GOOGLE_CSE_ID']
+os.environ["GOOGLE_API_KEY"] = st.secrets['GOOGLE_API_KEY']
+
 
 
 # App framework
 st.title('TouchGrass.fyi')
-prompt = st.text_input('Input your location here to get some suggestions on what you can do outside, NERD')
+prompt = st.text_input('Input your location here to get some suggestions on what you can do outside')
 
 
 # Prompt templates
@@ -22,7 +28,7 @@ suggestions_template = PromptTemplate(
 
 script_template = PromptTemplate(
     input_variables = ['suggestions', 'wikipedia_research'],
-    template = 'give me an idea of some events happening around me based on these suggestions:{suggestions} while leveraging this wikipedia research:{wikipedia_research}'
+    template = 'give me an idea of some location specific activities near me based on these suggestions:{suggestions} while leveraging this wikipedia research:{wikipedia_research}'
 )
 
 # Memory 
@@ -35,11 +41,19 @@ llm = OpenAI(temperature=0.9)
 suggestions_chain = LLMChain(llm=llm, prompt = suggestions_template,verbose=True, output_key = 'suggestions', memory=suggestions_memory)
 script_chain = LLMChain(llm=llm, prompt = script_template,verbose=True, output_key = 'script', memory=script_memory )
 wiki = WikipediaAPIWrapper()
+search = GoogleSearchAPIWrapper()
+tool = Tool(
+    name="Google Search",
+    description="Search Google for recent results.",
+    func=search.run,
+)
+
 
 #show stuff to the screen if there's a prompt
 if prompt: 
     suggestions = suggestions_chain.run(prompt)
     wiki_research = wiki.run(prompt)
+    tool.run(prompt)
     script = script_chain.run(suggestions=suggestions, wikipedia_research=wiki_research)
     
 
