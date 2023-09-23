68b242be54fed14cef334a9068f1ef562ac5b711
Osman <osman.humayun@gmail.com>
2023-09-23 16:16:26 -0700

    update 1

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain, SequentialChain
 from langchain.memory import ConversationBufferMemory
+from langchain.utilities import WikipediaAPIWrapper
 from langchain.tools import Tool
 from langchain.utilities import GoogleSearchAPIWrapper
 
@@ -14,43 +15,47 @@ os.environ["GOOGLE_API_KEY"] = st.secrets['GOOGLE_API_KEY']
 # App framework
 st.title('TouchGrass.fyi')
 prompt = st.text_input('Input your location here to get some suggestions on what you can do outside')
-categories = ["Sports", "Arts & Theatre", "Family", "Nature"]
+
+categories = ["Sports", "Arts & Theatre", "Comedy", "Family", "Nature"]
 selected_categories = st.multiselect("Select categories of activities you're interested in:", categories)
+custom_category = st.text_input("Enter your own category if it's not listed above:")
+
+if custom_category:
+    selected_categories.append(custom_category)
+
 touch_grass_button = st.button('Touch Grass')
 
 # Prompt templates
 suggestions_template = PromptTemplate(
     input_variables=['location', 'selected_categories'],
-    template='Given that I\'m interested in {selected_categories}, provide one suggestion on how I can spend time outdoors if I live in {location}'
+    template='Given that Im interested in {selected_categories}, provide suggestions on how I can spend time outdoors if I live in {location}'
+)
+
+script_template = PromptTemplate(
+    input_variables=['suggestions', 'wikipedia_research'],
+    template='give me an idea of some location specific activities near me based on these suggestions:{suggestions} while leveraging this wikipedia research:{wikipedia_research}'
 )
 
 # Memory 
-suggestions_memory = ConversationBufferMemory(input_key='location', memory_key='chat history')
+suggestions_memory = ConversationBufferMemory(input_key= 'location', memory_key= 'chat history')
+script_memory = ConversationBufferMemory(input_key= 'suggestions', memory_key= 'chat history')
 
 # Llms
 llm = OpenAI(temperature=0.9)
 suggestions_chain = LLMChain(llm=llm, prompt=suggestions_template, verbose=True, output_key='suggestions', memory=suggestions_memory)
+script_chain = LLMChain(llm=llm, prompt=script_template, verbose=True, output_key='script', memory=script_memory)
+wiki = WikipediaAPIWrapper()
 search = GoogleSearchAPIWrapper()
 
-def google_search_for_suggestion(suggestion):
-    results = search.results(suggestion, 1)  # Get top result
-    return results[0]['Link'] if results and results[0].get('Link') else None
-
+# Execute when button is clicked
 if touch_grass_button:
     if prompt and selected_categories:
         formatted_categories = ', '.join(selected_categories)
-        suggestion = suggestions_chain.run(location=prompt, selected_categories=formatted_categories)
-
-        # If the model returns multiple suggestions (in a list), take only the first one
-        if isinstance(suggestion, list):
-            suggestion = suggestion[0]
-
-        google_link = google_search_for_suggestion(suggestion)
-
-        st.write(suggestion)  # Display the suggestion
-        if google_link:
-            st.markdown(f"[Link to top search result]({google_link})")  # Display the link as a clickable hyperlink
-
-        # Display other info
-        with st.expander('Suggestions History'):
-            st.info(suggestions_memory.buffer)
+        suggestions = suggestions_chain.run(location=prompt, selected_categories=formatted_categories)
+        
+        wiki_research = wiki.run(prompt)
+        script = script_chain.run(suggestions=suggestions, wikipedia_research=wiki_research)
+    
+        st.write(suggestions)
+        
+       
